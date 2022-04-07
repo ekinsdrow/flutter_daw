f1185c3be5f881f48127a5bc0b5e2440fcfd1ac2
Ivan Kolchev <ekinsdrow@gmail.com>
2022-04-07 15:23:22 +0300

    Create simple layouting

--- lib/features/main/widgets/main_page.dart
+++ lib/features/main/widgets/main_page.dart
@@ -5,12 +5,98 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text('Main'),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                const _Header(),
+                const _TimeLine(),
+                Expanded(
+                  child: Row(
+                    children: const [
+                      _Mixer(),
+                      Expanded(
+                        child: _Tracks(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+             Positioned(
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              child: const _PianoRoll(),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      color: Colors.red[200],
+    );
+  }
+}
+
+class _TimeLine extends StatelessWidget {
+  const _TimeLine({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 35,
+      color: Colors.green[200],
+    );
+  }
+}
+
+class _Mixer extends StatelessWidget {
+  const _Mixer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: double.infinity,
+      color: Colors.blue[200],
+    );
+  }
+}
+
+class _Tracks extends StatelessWidget {
+  const _Tracks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      color: Colors.yellow[200],
+    );
+  }
+}
+
+class _PianoRoll extends StatelessWidget {
+  const _PianoRoll({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      color: Colors.black38,
     );
   }
 }
